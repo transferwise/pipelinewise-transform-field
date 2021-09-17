7d68e7b36255ca23a1344a35d399791835efd2a3
Samira El Aabidi <54845154+Samira-El@users.noreply.github.com>
2021-09-17 10:15:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='pipelinewise-transform-field',
       extras_require={
         'test': [
             'pytest==6.2.*',
-            'pytest-cov=2.12.*',
+            'pytest-cov==2.12.*',
             'pylint==2.7.*',
         ]
       },
